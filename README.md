5d5e812bae221cb8787f1ed4b60c308da3fcf147
thiagosilvagauge <thiago.silva@gauge.com.br>
2024-02-04 00:35:20 -0300

    create Users and Surveys

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # survey
+# Authors: Thiago Oliveira e Gabriel Gonçalves
+# Planejamento do projeto
+
+# Ordem de execução
+
+# 1 - Criação de Usuário
+# 1.1 - Criação do model
+# 1.2 - Criação do controller
+# 1.3 - Criação da rota
+# 1.4 - Configurar rota global
+# 1.5 - Configurar servidor
+# 1.6 - Configurar Banco de dados(Controller)
+
+# 2 - Criação de Pesquisa
+
+# 3 - Criação de Pergunta
+
+# 4 - Criação de Resposta
+
+# 5 - Criação de Resposta Usuário
+
+# 6 - Criação de Pesquisa Usuário
+
+# 7 - Criação de Relatório
+
+# 8 - Criação de Relatório Pesquisa
+
--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const compression = require("compression");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.send("Seja Bem vindo");
-});
+const UserRourter = require("./routes/UserRoute");
+const SurveyRouter = require("./routes/SurveyRoute");
+
+app.use(compression({ filter: (req, res) => true, brotli: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/user", UserRourter);
+app.use("/api/survey", SurveyRouter);
+
 
 app.listen(PORT, () => {
     console.log(`Sevidor rodando em http://localhost:${PORT}`);
