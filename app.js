17a3096790557da0c179869585e5f1ee04299e38
thiagosilvagauge <thiago.silva@gauge.com.br>
2024-02-04 14:33:51 -0300

    add classes

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const PORT = process.env.PORT || 3000;
 
 const UserRourter = require("./routes/UserRoute");
 const SurveyRouter = require("./routes/SurveyRoute");
+const QuestionRouter = require("./routes/QuestionRoute");
+const AnswerRouter = require("./routes/AnswerRoute");
+const SurveyUserRouter = require("./routes/SurveyUserRoute");
 
 app.use(compression({ filter: (req, res) => true, brotli: true }));
 app.use(express.json());
@@ -12,6 +15,9 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/user", UserRourter);
 app.use("/api/survey", SurveyRouter);
+app.use("/api/question", QuestionRouter);
+app.use("/api/answer", AnswerRouter);
+app.use("/api/surveyuser", SurveyUserRouter);
 
 
 app.listen(PORT, () => {
